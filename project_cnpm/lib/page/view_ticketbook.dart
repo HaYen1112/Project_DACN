dd4162905a23d0089c3271b5c70d1eb1a6d25979
truongvanxinh <18130292@st.hcmuaf.edu.vn>
2022-05-22 08:33:24 +0700

    Push code trang chủ với menu

---
+++ project_cnpm/lib/page/view_ticketbook.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:project_cnpm/widget/navigation_drawer.dart';
+class ViewTicket extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    drawer: NaigationDrawerWidget(),
+    appBar: AppBar(
+      title: Text('Xem vé đã đặt'),
+      centerTitle: true,
+      backgroundColor: Color.fromARGB(255, 248, 178, 29),
+    ),
+  );
+}
