c78830f1fda05330c28b9fe30a56134320faf066
Hoàng Vinh <73897533+18130284@users.noreply.github.com>
2022-06-04 02:19:44 +0700

    push lại file bị lỗi

--- flutter_app1/android/app/src/main/java/com/flutter/app/flutter_app1/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.flutter.app.flutter_app1;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ project_cnpm/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:project_cnpm/widget/navigation_drawer.dart';
+import 'package:project_cnpm/widget/navigation_manage_drawer.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:project_cnpm/page/login.dart';
+
+void main() async{
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  static const appTitle = 'TicketBook';
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: appTitle,
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+    _MainPageState createState() => _MainPageState();
+}
+class _MainPageState extends State<MainPage>{
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    drawer: NavigationDrawerWidget(),
+    appBar: AppBar(
+      title: Text(NavigationDrawerWidget.appTitle),
+      backgroundColor: Color.fromARGB(255, 248, 178, 29),
+    ),
+  );
+}
