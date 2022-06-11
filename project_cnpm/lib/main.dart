54b1e46a732bcba9ae1706cf702017cd79c0468a
unknown <18130293@st.hcmuaf.edu.vn>
2022-06-11 19:35:57 +0700

    Update Regist UI

---
+++ project_cnpm/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:project_cnpm/page/registration_page.dart';
+import 'package:project_cnpm/widget/navigation_drawer.dart';
+import 'package:project_cnpm/widget/navigation_manage_drawer.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:project_cnpm/page/login.dart';
+
+
+void main() async{
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  static const appTitle = 'TicketBook';
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: appTitle,
+      home: RegistrationPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+    _MainPageState createState() => _MainPageState();
+}
+class _MainPageState extends State<MainPage>{
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    drawer: NavigationDrawerWidget(),
+    appBar: AppBar(
+      title: Text(NavigationDrawerWidget.appTitle),
+      backgroundColor: Color.fromARGB(255, 248, 178, 29),
+    ),
+  );
+}
