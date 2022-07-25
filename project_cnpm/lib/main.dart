838232b8fe9ba0c1bafa2bc8ac90e5d87de70d44
hayen1112 <18130295@st.hcmuaf.edu.vn>
2022-07-25 11:50:05 +0700

    update code

--- project_cnpm/lib/main.dart
+++ project_cnpm/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:project_cnpm/page/add_trip_manager.dart';
 import 'package:project_cnpm/page/listTripPage.dart';
 import 'package:project_cnpm/page/Manager_trip.dart';
 import 'package:project_cnpm/page/auth_page.dart';
+import 'package:project_cnpm/page/search_page.dart';
 import 'package:project_cnpm/page/utils.dart';
 import 'package:project_cnpm/page/verify_email_page.dart';
 import 'package:project_cnpm/page/TicketBook.dart';
@@ -40,9 +41,6 @@ class MyApp extends StatelessWidget {
       navigatorKey: navigatorKey,
       debugShowCheckedModeBanner: false,
       title: appTitle,
-
-      home: ListTrip(),
-
       home: SearchScreen(),
 
 
